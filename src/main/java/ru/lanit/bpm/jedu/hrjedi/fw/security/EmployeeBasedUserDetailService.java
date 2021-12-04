165260c3c0cf11f710db2c6a11e3bb5aa30d6404
Artem Bocharov <Bocharov@lanit.ru>
2021-12-04 17:05:36 +0300

    EDU-29778 Перевести приложение hr-jedi-back на Каркас 2.0

---
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/fw/security/EmployeeBasedUserDetailService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2008-2020
+ * LANIT
+ * All rights reserved.
+ *
+ * This product and related documentation are protected by copyright and
+ * distributed under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or related documentation may be
+ * reproduced in any form by any means without prior written authorization of
+ * LANIT and its licensors, if any.
+ *
+ * $
+ */
+package ru.lanit.bpm.jedu.hrjedi.fw.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.EmployeeService;
+import ru.lanit.bpm.jedu.hrjedi.domain.Employee;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EmployeeBasedUserDetailService implements UserDetailsService {
+    private EmployeeService employeeService;
+
+    public EmployeeBasedUserDetailService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) {
+        try {
+            Employee user = employeeService.findByLogin(username);
+            return UserPrinciple.build(user);
+        } catch (EntityNotFoundException e) {
+            throw new UsernameNotFoundException("User not found", e);
+        }
+    }
+}
